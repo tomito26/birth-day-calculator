5dd367f6c78307b47b3c62af68b59e6a54e94600
tomito26 <tommybwah@gmail.com>
2020-07-19 03:49:57 +0300

    modified the javascript

--- js/script.js
+++ js/script.js
@@ -1,44 +1,24 @@
 function nameCalculator(){
-    let date = document.getElementById('date').value;
-    let year = document.getElementById('year').value;
-    let month = parseInt(document.getElementById('month').value);
-    let gender = document.getElementById('gender').value;
+    let d = document.getElementById('date').value;
+    let  year = d.split('-')[0]
+    let month = parseInt(d.split('-')[1])
+    let date = parseInt(d.split('-')[2])
     let cc = parseInt(year.slice(0,2))
     let yy = parseInt(year.slice(2,4))
+    let output = Math.trunc(( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + date) % 7)
     
-    let output = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + date ) % 7
-    let maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-    let femaleName = ['Akosua', 'Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
-
-
-    if (date<=0 || date>31){
-        alert('Invalid date')
-    }
-    else if(month<=0 || month>12){
-        alert('Invalid month')
-    }
-    else if(month==2 && date > 29){
-        alert('Invalid date of month')
+    let gender = document.getElementById('gender').value;
+    let day = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+    let maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
+    let femaleNames = ['Akosua','Adwoa',' Abenaa','Akua','Yaa','Afua','Ama']
+    if (gender == 'male'){
+        document.write('You were born on '+ day[output] +'  and your akan name is ' + maleNames[output])
+    } 
+    else{
+        alert (femaleNames[output])
     }
 
+     
 
-  
-
-
-
-    
-// if (month =>1|| month <=12){
-//     nameCalculator(month)
-// }
-// else{
-//     alert('Wrong input')
-// }
-
-// if(date<=1 || date>= 31){
-//     nameCalculator(date)
-// }
-// else{
-//     alert('wrong input')
-// }
 
 }
